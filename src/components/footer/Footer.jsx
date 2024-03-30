2eb98de896dc4a581d9d88da0ba67dfcb3b5881a
Abril31 <brivaldi0416@gmail.com>
2024-03-30 19:11:04 -0400

    Detalles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="bg-base flex text-white p-2 pt-7 pb-7 place-content-evenly font-bold text-xl mt-10">
-      <h1 className>About Us</h1>
+      <h1>About Us</h1>
       <h2>Events in Top Cities</h2>
       <h2>Follow Us</h2>
       <h2>Contact</h2>
