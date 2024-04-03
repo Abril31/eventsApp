14d3bf0b9beb420cf2df7adf7ccd0b147bc964fa
Abril31 <brivaldi0416@gmail.com>
2024-04-03 17:42:11 -0400

    hasrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./queryClient.js";
@@ -10,10 +10,10 @@ import queryClient from "./queryClient.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
         <ReactQueryDevtools initialIsOpen={false} />
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </React.StrictMode>
 );
