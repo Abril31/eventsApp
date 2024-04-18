601c43a94a3f776af5616b8efc559b148518e5aa
Abril31 <brivaldi0416@gmail.com>
2024-04-18 12:36:37 -0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["@stripe/stripe-js"],
+      external: ["@stripe/stripe-js", "fsevents"],
     },
   },
 });
